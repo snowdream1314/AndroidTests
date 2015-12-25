2dc6342d04e0a56d7ee7fb6feb2586a9f01251d8
snowdream1314 <595968602@qq.com>
2015-12-25 10:53:35 +0800

    add bindService and startForeground

--- ServiceTest/src/com/example/servicetest/MainActivity.java
+++ ServiceTest/src/com/example/servicetest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.servicetest;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,14 +16,36 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	private Button startService;
 	private Button stopService;
+	private Button bindService;
+	private Button unbindService;
+	
+	private MyService.DownloadBinder downloadBinder;
+	
+	private ServiceConnection connection  = new ServiceConnection() {
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			downloadBinder = (MyService.DownloadBinder) service;
+			downloadBinder.startDownload();
+			downloadBinder.getProgress();
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		startService = (Button) findViewById(R.id.start_service);
 		stopService = (Button) findViewById(R.id.stop_service);
+		bindService = (Button) findViewById(R.id.bind_service);
+		unbindService = (Button) findViewById(R.id.stop_service);
 		startService.setOnClickListener(this);
 		stopService.setOnClickListener(this);
+		bindService.setOnClickListener(this);
+		unbindService.setOnClickListener(this);
 		
 	}
 	
@@ -34,6 +59,13 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.stop_service:
 			Intent stopIntent = new Intent(this, MyService.class);
 			stopService(stopIntent);//停止服务
+			break;
+		case R.id.bind_service:
+			Intent bindIntent = new Intent(this, MyService.class);
+			bindService(bindIntent, connection, BIND_AUTO_CREATE);//绑定服务
+			break;
+		case R.id.unbind_service:
+			unbindService(connection);//解绑服务
 			break;
 		default:
 			break;
--- ServiceTest/src/com/example/servicetest/MyService.java
+++ ServiceTest/src/com/example/servicetest/MyService.java
@@ -1,20 +1,42 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
-
+	
+	private DownloadBinder mBinder = new DownloadBinder();
+	
+	class DownloadBinder extends Binder {
+		
+		public void startDownload() {
+			Log.d("MyService", "startDownload executed");
+		}
+		
+		public int getProgress() {
+			Log.d("MyService", "getProgress executed");
+			return 0;
+		}
+	}
+	
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		return mBinder;
 	}
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Notification notification = new Notification(R.drawable.ic_launcher, "Notification comes", System.currentTimeMillis());
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(this, "This is a Title", "This is content", pendingIntent);
+		startForeground(1, notification);
 		Log.d("MyServcice", "onCreate executed");
 	}
 	
