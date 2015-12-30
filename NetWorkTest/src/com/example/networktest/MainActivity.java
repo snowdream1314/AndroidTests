c2b0793964bcfe2d6ef8fc98ed9930637859b16e
snowdream1314 <595968602@qq.com>
2015-12-30 18:00:20 +0800

    update NetWorktest

--- NetWorkTest/src/com/example/networktest/MainActivity.java
+++ NetWorkTest/src/com/example/networktest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.networktest;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -12,6 +13,8 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -23,11 +26,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
 	
 	private Button sendRequest;
 	private TextView responseText;
+	private String texts;
+	private String text;
+	private int i;
 	
 	private static final int SHOW_RESPONSE = 0;
 	
@@ -50,6 +57,8 @@ public class MainActivity extends Activity implements OnClickListener{
 		sendRequest = (Button) findViewById(R.id.send_request);
 		responseText = (TextView) findViewById(R.id.response);
 		sendRequest.setOnClickListener(this);
+//		texts = null;
+//		text = null;
 	}
 	
 	@Override
@@ -67,7 +76,7 @@ public class MainActivity extends Activity implements OnClickListener{
 			public void run() {
 				HttpURLConnection connection = null;
 				try {
-					URL url = new URL("http://www.baidu.com");
+					URL url = new URL("http://mobile.weather.com.cn/js/citylist.xml");
 					connection = (HttpURLConnection) url.openConnection();
 					connection.setRequestMethod("GET");
 					//post请求
@@ -84,13 +93,13 @@ public class MainActivity extends Activity implements OnClickListener{
 					while((line = reader.readLine()) != null) {
 						response.append(line);
 					}
-					Message message = new Message();
-					message.obj = response.toString();
-					message.what = SHOW_RESPONSE;
-					//将服务器返回的结果存入Message
-					handler.sendMessage(message);
+//					Message message = new Message();
+//					message.obj = response.toString();
+//					message.what = SHOW_RESPONSE;
+//					//将服务器返回的结果存入Message
+//					handler.sendMessage(message);
 					
-//					parseXMLWithPull(response);//解析XML
+					parseXMLWithPull(response.toString());//解析XML
 					
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -103,9 +112,6 @@ public class MainActivity extends Activity implements OnClickListener{
 		}).start();
 	}
 	
-//	private void parseXMLWithPull(String XMLdata) {
-//		
-//	}
 	
 	private void sendRequestWithHttpClient() {
 		//开启线程发起网络请求
@@ -115,17 +121,18 @@ public class MainActivity extends Activity implements OnClickListener{
 				HttpURLConnection connection = null;
 				try {
 					HttpClient httpClient = new DefaultHttpClient();
-					HttpGet httpGet = new HttpGet("http://www.baidu.com");
+					HttpGet httpGet = new HttpGet("http://mobile.weather.com.cn/js/citylist.xml");
 					HttpResponse httpResponse = httpClient.execute(httpGet);
 					if (httpResponse.getStatusLine().getStatusCode() == 200) {
 						//请求成功
 						HttpEntity entity = httpResponse.getEntity();
 						String response = EntityUtils.toString(entity, "utf-8");
-						Message message = new Message();
-						message.obj = response.toString();
-						message.what = SHOW_RESPONSE;
-						//将服务器返回的结果存入Message
-						handler.sendMessage(message);
+						parseXMLWithPull(response);
+//						Message message = new Message();
+//						message.obj = response.toString();
+//						message.what = SHOW_RESPONSE;
+//						//将服务器返回的结果存入Message
+//						handler.sendMessage(message);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -133,6 +140,56 @@ public class MainActivity extends Activity implements OnClickListener{
 			}
 		}).start();
 	}
+	
+	private void parseXMLWithPull(String xmlData) {
+		try {
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			XmlPullParser xmlPullParser = factory.newPullParser();
+			xmlPullParser.setInput(new StringReader(xmlData));
+			int eventType = xmlPullParser.getEventType();
+			String d1 = "";
+			String d2 = "";
+			String d3 = "";
+			String d4 = "";
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+				String nodeName = xmlPullParser.getName();
+				switch (eventType) {
+				//开始解析节点
+				case XmlPullParser.START_TAG: {
+//					texts = null;
+//					i = 0;
+					if ("d".equals(nodeName)) {
+						d1 = xmlPullParser.getAttributeValue(0);
+						d2 = xmlPullParser.getAttributeValue(1);
+						d3 = xmlPullParser.getAttributeValue(2);
+						d4 = xmlPullParser.getAttributeValue(3);
+						text = d1 + "," + d2 + "," + d3 + "," + d4;
+						texts = texts + "\n" + text;
+					}
+					break;
+				}
+				case XmlPullParser.END_TAG: {
+					if ("c".equals(nodeName)) {
+//						responseText.setText(texts);
+						Message message = new Message();
+						message.obj = texts.toString();
+						message.what = SHOW_RESPONSE;
+						//将服务器返回的结果存入Message
+						handler.sendMessage(message);
+//						Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
+					}
+				}
+					break;
+				default:
+					break;
+				}
+				eventType = xmlPullParser.next();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
